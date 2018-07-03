716cbab1970e6bc362f58c3d640559fff0044f5e
jastr945 <polina.jastr@gmail.com>
2018-07-02 21:58:51 -0700

    Added find_locations()

--- elastic_wifi.py
+++ elastic_wifi.py
@@ -1,58 +1,54 @@
-from datetime import datetime
+import time
 from elasticsearch import Elasticsearch
+from geopy.geocoders import Nominatim
 
 es = Elasticsearch()
+geolocator = Nominatim()
 
-# This is just a test
-doc = {"address": "Shanghai Tunnel, 211, Southwest Ankeny Street, Chinatown, Old Town, Portland, Multnomah County, Oregon, 97204, United States of America", "location": "45.5226076,-122.672583510967"}
 
-mapping = {
-        "mappings": {
-            "doc": {
-                "properties": {
-                    "address": {
-                        "type": "text"
+def find_locations():
+    """Taking user input, converting into coordinates and searching through the Elasticsearch index to find the closest WiFi spots."""
+
+    street = input("Enter your location: ")
+    current_address = street + ", Portland"
+    try:
+        coordinates = geolocator.geocode(current_address, timeout=None)
+        print("Your coordinates: {}, {}...".format(coordinates.latitude, coordinates.longitude))
+
+        time.sleep(2)
+        print("Searching for free WiFi spots near you...")
+        time.sleep(3)
+
+        # search through index using the Geo Distance Query
+        search_body = {
+            "query": {
+                "bool" : {
+                    "must" : {
+                        "match_all" : {}
                     },
-                    "location": {
-                        "type": "geo_point"
+                    "filter" : {
+                        "geo_distance" : {
+                            "distance" : "0.2mi",
+                            "location" : {
+                                "lat" : coordinates.latitude,
+                                "lon" : coordinates.longitude
+                            }
+                        }
                     }
                 }
             }
         }
-    }
+
+        res = es.search(index="wifi-index", body=search_body)
+
+        print("Got %d Hits:" % res['hits']['total'])
+        for hit in res['hits']['hits']:
+            print(hit["_source"], "\n")
+
+    except AttributeError:
+        print("Couldn't find coordinates for this address: {}".format(current_address))
+
+    return("Search finished.")
 
 
-# create index
-# es.indices.create(index="wifi-index", body=mapping)
-# es.index(index="wifi-index", doc_type="doc", body=doc)
-
-
-# search through index using the Geo Distance Query
-search_body = {
-    "query": {
-        "bool" : {
-            "must" : {
-                "match_all" : {}
-            },
-            "filter" : {
-                "geo_distance" : {
-                    "distance" : "5km",
-                    "location" : {
-                        "lat" : 45.60,
-                        "lon" : -122.68
-                    }
-                }
-            }
-        }
-    }
-}
-
-res = es.search(index="wifi-index", body=search_body)
-
-print("Got %d Hits:" % res['hits']['total'])
-for hit in res['hits']['hits']:
-    print(hit["_source"], "\n")
-
-
-# # delete index
-# es.indices.delete(index='wifi-index', ignore=[400, 404])
+print(find_locations())
--- get_locations.py
+++ get_locations.py
@@ -1,9 +1,30 @@
 import requests
 import re
 from geopy.geocoders import Nominatim
+from elasticsearch import Elasticsearch
 
 
+es = Elasticsearch()
 geolocator = Nominatim()
+
+# Creating the mapping with a Geo-Point
+mapping = {
+        "mappings": {
+            "doc": {
+                "properties": {
+                    "address": {
+                        "type": "text"
+                    },
+                    "location": {
+                        "type": "geo_point"
+                    }
+                }
+            }
+        }
+    }
+
+# creating index
+es.indices.create(index="wifi-index", body=mapping)
 
 
 def get_wifi():
@@ -17,6 +38,9 @@ def get_wifi():
     result = single_commas.split(",Been here? ,Add a comment...,")
 
     for r in result:
+
+        doc = {}
+
         addresscut = r.split(",")
         for a in addresscut:
             if "..." in a:
@@ -25,25 +49,33 @@ def get_wifi():
 
         try:
             location = geolocator.geocode(address_total, timeout=None)
-            c.execute("INSERT INTO wifi VALUES (?, ?, ?)", (location.address, location.latitude, location.longtitude)
-            conn.commit()
+            doc["address"] = location.address
+            doc["location"] = str(location.latitude) + "," + str(location.longtitude)
+            es.index(index="wifi-index", doc_type="doc", body=doc)  # adding the data into the Elastcsearch index
             print("{} was saved into the database".format(addresscut[0]))
         except AttributeError:
             print("Couldn't convert this one: {}".format(address_total))
-    conn.close()
+
     return("Finished manipulating data.")
 
 
-# def populate_db():
-#     """Populates the database from file, in case geopy throws timeout error."""
-#
-#     data = {}
-#     f = open("wifi.txt", "r")
-#     for line in f:
-#         address = line.split("of America ")[0]
-#         lat = (line.split("of America ")[1]).split(" ")[0]
-#         lng = ((line.split("of America ")[1]).split(" ")[1]).split("\n")[0]
-#         data["address"] = address
-#         data["lat"] = lat
-#         data["lng"] = lng
-#         print(json.dumps(data))
+def populate_db():
+    """Populates the database from file, in case geopy throws timeout error."""
+
+    f = open("wifi.txt", "r")
+    for line in f:
+        doc = {}
+        address = line.split("of America ")[0]
+        lat = (line.split("of America ")[1]).split(" ")[0]
+        lng = ((line.split("of America ")[1]).split(" ")[1]).split("\n")[0]
+        location = lat + "," + lng
+        doc["address"] = address
+        doc["location"] = location
+        es.index(index="wifi-index", doc_type="doc", body=doc)  # adding the data into the Elastcsearch index
+    return("Finished populating wifi-index.")
+
+
+print(populate_db())
+
+# # delete index
+# es.indices.delete(index='wifi-index', ignore=[400, 404])
