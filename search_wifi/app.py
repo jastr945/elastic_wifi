6c71eed4442e65e45fc39f7fe635e73f4705a841
jastr945 <polina.jastr@gmail.com>
2018-07-07 22:15:34 -0700

    Serverless Flask connects to AWS Elasticsearch Service

---
+++ search_wifi/app.py
@@ -0,0 +1,52 @@
+import boto3
+from chalice import Chalice
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from geopy.geocoders import Nominatim
+
+app = Chalice(app_name='search_wifi')
+
+host = 'search-elastic-wifi2-tj7nwqymkpo4rxoe3mpdvpxjqi.us-west-2.es.amazonaws.com'
+region = 'us-west-2'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+es = Elasticsearch(
+    hosts = [{'host': host, 'port': 443}],
+    http_auth = awsauth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection
+)
+
+geolocator = Nominatim()
+
+example_address = "110 nw 10th ave"
+
+@app.route('/')
+def index():
+    street = example_address
+    current_address = (street + ", Portland").upper()
+    coordinates = geolocator.geocode(current_address, timeout=None)
+
+    search_body = {
+        "query": {
+            "bool" : {
+                "must" : {
+                    "match_all" : {}
+                },
+                "filter" : {
+                    "geo_distance" : {
+                        "distance" : "0.2mi",
+                        "location" : {
+                            "lat" : coordinates.latitude,
+                            "lon" : coordinates.longitude
+                        }
+                    }
+                }
+            }
+        }
+    }
+    res = es.search(index="wifi-index", size=100, body=search_body)
+    return res
